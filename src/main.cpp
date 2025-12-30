bbcfb9770d1f81dd118fcab0b526f85f944f5d69
mdawso <dawsonmathew08@gmail.com>
2025-12-30 21:16:08 +0000

    Moved globals to appstate

--- src/main.cpp
+++ src/main.cpp
@@ -14,28 +14,27 @@
 
 using Complex = std::complex<long double>;
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+struct WindowBounds {
+    int win_x;
+    int win_y;
+    double view_min_x;
+    double view_min_y;
+    double view_max_x;
+    double view_max_y;
+};
 
-constexpr int WINDOW_X = 1152;
-constexpr int WINDOW_Y = 648;
+struct State {
+    WindowBounds* bounds;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* mandelbrot_texture;
+};
 
-constexpr double VIEW_MIN_X = -2.0;
-constexpr double VIEW_MAX_X = 1.0;
-constexpr double VIEW_MIN_Y = -1.0;
-constexpr double VIEW_MAX_Y = 1.0;
-constexpr int MAX_ITERATIONS = 100;
-
-static std::array<SDL_Color, WINDOW_X * WINDOW_Y> pixel_buffer;
-constexpr int NUM_THREADS = 8;
-static std::array<std::thread, NUM_THREADS> workers;
-
-Complex map_pixel_to_complex(const int x, const int y) {
-    double real = VIEW_MIN_X + (double)x / WINDOW_X * (VIEW_MAX_X - VIEW_MIN_X);
-    double imaginary = VIEW_MAX_Y - (double)y / WINDOW_Y * (VIEW_MAX_Y - VIEW_MIN_Y);
+Complex map_pixel_to_complex(const int x, const int y, WindowBounds* bounds) {
+    double real = bounds->view_min_x + (double)x / bounds->win_x * (bounds->view_max_x - bounds->view_min_x);
+    double imaginary = bounds->view_max_y - (double)y / bounds->win_y * (bounds->view_max_y - bounds->view_min_y);
     return Complex(real, imaginary);
 }
-
 
 int diverges(const Complex& c, int max_iterations) {
     Complex z(0.0, 0.0);
@@ -48,35 +47,45 @@ int diverges(const Complex& c, int max_iterations) {
     return max_iterations;
 }
 
-SDL_Color map_iters_to_colour(int iters, int max_iterations) {
-    unsigned char shade = std::floor(std::lerp(0.0,255.0,static_cast<double>(iters)/max_iterations));
-    //if (iters == max_iterations) shade = 0;
-    SDL_Color col = {
-        .r = shade,
-        .g = 0,
-        .b = 0,
-        .a = 255
-    };
-    return col;
-}
+void recalculate_mandelbrot_texture(State* state, int max_iterations) {
+    size_t buffer_size = state->bounds->win_x * state->bounds->win_y;
+    std::vector<SDL_Color> pixel_buffer(buffer_size);
 
-void calculate_pixel_buffer(int start_i, int end_i) {
-    if (end_i > pixel_buffer.size()) return;
-    for (int i = start_i; i < end_i; i++) {
-        int x = i % WINDOW_X;
-        int y = i / WINDOW_X;
-        Complex c = map_pixel_to_complex(x,y);
-        int iters = diverges(c, MAX_ITERATIONS);
-        pixel_buffer[i] = map_iters_to_colour(iters, MAX_ITERATIONS);
+    constexpr SDL_Color BLACK = {0,0,0,255};
+
+    for (int i = 0; i < buffer_size; i++) {
+        int x = i % state->bounds->win_x;
+        int y = i / state->bounds->win_x;
+        Complex c = map_pixel_to_complex(x, y, state->bounds);
+        int d = diverges(c, max_iterations);
+        unsigned char r = (d * 255) / max_iterations;
+        pixel_buffer[i] = {r, 0, 0, 255};
     }
+
+    SDL_UpdateTexture(state->mandelbrot_texture, NULL, pixel_buffer.data(), state->bounds->win_x * sizeof(SDL_Color));
 }
 
-void work(int start_i, int end_i) {
-    calculate_pixel_buffer(start_i, end_i);
-}
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
 {
+    WindowBounds* bounds = new WindowBounds{
+        .win_x = 1280,
+        .win_y = 720,
+        .view_min_x = -2.0,
+        .view_min_y = -1.0,
+        .view_max_x = 1.0,
+        .view_max_y = 1.0
+    };
+
+    State* state = new State{
+        .bounds = bounds,
+        .window = NULL,
+        .renderer = NULL,
+        .mandelbrot_texture = NULL,
+    };
+
+    *appstate = state;
+
     SDL_SetAppMetadata("mandelbrot", "1.0", "com.mias.mandelbrot");
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -84,34 +93,26 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("mandelbrot", WINDOW_X, WINDOW_Y, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("mandelbrot", bounds->win_x, bounds->win_y, SDL_WINDOW_RESIZABLE, &state->window, &state->renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    SDL_SetRenderLogicalPresentation(renderer, WINDOW_X, WINDOW_Y, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    
+    SDL_SetRenderLogicalPresentation(state->renderer, bounds->win_x, bounds->win_y, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
-    constexpr SDL_Color BLACK = {0,0,0,SDL_ALPHA_OPAQUE};
-    pixel_buffer.fill(BLACK);
-
-    for (int i = 0; i < NUM_THREADS; ++i) {
-        int start_i = i * pixel_buffer.size() / NUM_THREADS;
-        int end_i = (i + 1) * pixel_buffer.size() / NUM_THREADS;
-        if (i == NUM_THREADS - 1) {
-            end_i = pixel_buffer.size();
-        }
-        workers[i] = std::thread(work, start_i, end_i);
+    state->mandelbrot_texture = SDL_CreateTexture(state->renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, state->bounds->win_x, state->bounds->win_y);
+    if (!state->mandelbrot_texture) {
+        SDL_Log("Couldn't create mandelbrot_texture: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    for (auto &w : workers) {
-        if (w.joinable()) {
-            w.join();
-        }
-    }
+    constexpr int max_iters = 250;
+    recalculate_mandelbrot_texture(state, max_iters);
 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
@@ -119,32 +120,25 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     return SDL_APP_CONTINUE;
 }
 
-static unsigned int frame = 0;
-SDL_AppResult SDL_AppIterate(void *appstate)
+SDL_AppResult SDL_AppIterate(void* appstate)
 {
 
-    std::cout << frame++ << '\n';
+    State* state = static_cast<State*>(appstate);
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(state->renderer);
 
-    for (int i = 0; i < pixel_buffer.size(); i++) {
-        int x = i % WINDOW_X;
-        int y = i / WINDOW_X;
-        SDL_SetRenderDrawColor(
-            renderer,
-            pixel_buffer[i].r,
-            pixel_buffer[i].g,
-            pixel_buffer[i].b,
-            pixel_buffer[i].a
-        );
-        SDL_RenderPoint(renderer,x,y);
-    }
+    SDL_RenderTexture(state->renderer, state->mandelbrot_texture, NULL, NULL);
 
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(state->renderer);
     return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
+    State* state = static_cast<State*>(appstate);
+    if (state) {
+        delete state->bounds;
+        delete state;
+    }
 }
